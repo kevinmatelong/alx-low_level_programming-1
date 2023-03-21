191c0b22df1f9541e22379d5530eaf0d05c569b6
limobr <limobrian48@gmail.com>
2023-03-21 08:57:58 -0700

    nested

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,47 @@
 #include "main.h"
 
-/*
- * print_times_table - Entry point
- *
- * Return: void
- *
- * prints times table
- *
- */void print_times_table(int n)
+/**
+ * print_times_table - prints the n times table,starting
+ * with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
 {
-	int row, column, result;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (row = 0; row <= n; row++)
+		for (i = 0; i <= n; i++)
 		{
-			for (column = 0; column <= n; column++)
+			for (j = 0; j <= n; j++)
 			{
-				result = (row * column);
-				if (column == 0)
-					_putchar('0' + result);
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					if (result <= 9)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar('0' + result);
-					}
-					else if (result > 9 && result < 100)
-					{
-						_putchar(' ');
-						_putchar('0' + (result / 10));
-						_putchar('0' + (result % 10));
-					}
-					else if (result >= 100)
-					{
-						_putchar('0' + (result / 100));
-						_putchar('0' + ((result / 10) % 10));
-						_putchar('0' + (result % 10));
-					}
-				}
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			} else if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10 && k < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			} else if (k >= 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 100) + '0');
+				_putchar(((k / 10) % 10) + '0');
+				_putchar((k % 10) + '0');
 			}
+		}
 			_putchar('\n');
 		}
 	}
