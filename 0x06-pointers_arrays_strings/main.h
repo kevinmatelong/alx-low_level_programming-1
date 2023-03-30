89c90222a92895c72d543fca771a641bac644c42
limobr <limobrian48@gmail.com>
2023-03-30 02:24:39 -0700

    pointers

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -48,4 +49,6 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void print_line(char *c, int s, int l);
+char *_strcat(char *dest, char *src);
+
 #endif
